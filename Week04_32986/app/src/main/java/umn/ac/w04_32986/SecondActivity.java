17d44f445dc8482aadd8d68dd332cce196d3e9f6
RafiSuryadani <rafisuryadani007@gmail.com>
2021-03-17 13:10:48 +0700

    Week04

---
+++ Week04_32986/app/src/main/java/umn/ac/w04_32986/SecondActivity.java
@@ -0,0 +1,15 @@
+package umn.ac.w04_32986;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        setTitle("00000032986");
+    }
+}
